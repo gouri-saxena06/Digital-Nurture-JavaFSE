f6d8e7bd9eb58cf41d8f23e495ef2993ddcd144c
Gouri Saxena <gourisaxena402@gmail.com>
2026-07-02 20:28:51 +0530

    Added Factory Method Pattern

---
+++ week 1/Factory Method Pattern/DocumentFactory.java
@@ -0,0 +1,16 @@
+public class DocumentFactory {
+
+    public static Document createDocument(String type) {
+
+        if(type.equalsIgnoreCase("pdf"))
+            return new PdfDocument();
+
+        if(type.equalsIgnoreCase("word"))
+            return new WordDocument();
+
+        if(type.equalsIgnoreCase("excel"))
+            return new ExcelDocument();
+
+        return null;
+    }
+}
---
+++ week 1/Factory Method Pattern/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Document doc =
+                DocumentFactory.createDocument("pdf");
+
+        doc.open();
+    }
+}
