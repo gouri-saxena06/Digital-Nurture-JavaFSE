aed02066b8e74b414a5d5a1e109c6d7722e8e353
Gouri Saxena <gourisaxena402@gmail.com>
2026-07-02 20:37:06 +0530

    Added Financial Forecasting

---
+++ week 1/Financial Forecasting/Forecast.java
@@ -0,0 +1,16 @@
+public class Forecast {
+
+    public static double futureValue(
+            double amount,
+            double rate,
+            int years) {
+
+        if(years == 0)
+            return amount;
+
+        return futureValue(
+                amount * (1 + rate),
+                rate,
+                years - 1);
+    }
+}
